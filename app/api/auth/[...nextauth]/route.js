0498afd575c7e96b3c0b06a72696970d8f65c41e
Syed Muhammad Muhtasham <muhtasham00@gmail.com>
2024-10-14 12:50:04 +0500

    fix

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,7 +10,7 @@ const authOptions = {
   providers: [
     FacebookProvider({
       clientId: process.env.FACEBOOK_CLIENT_ID,
-      clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET,
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
@@ -27,7 +27,7 @@ const authOptions = {
       name: "credentials",
       async authorize(credentials) {
         const { email, password } = credentials;
-        console.log(email)
+        console.log(email);
 
         try {
           await connectMongoDB();
@@ -41,12 +41,11 @@ const authOptions = {
             throw new Error("Password is incorrect");
           }
 
-         
           user.loginType = "credentials";
           await user.save();
 
           return {
-            id: user._id, 
+            id: user._id,
             email: user.email,
             name: user.name,
           };
@@ -66,35 +65,32 @@ const authOptions = {
   },
   callbacks: {
     async jwt({ token, user, account }) {
-      console.log(user)
+      console.log(user);
       if (user) {
-      
         token.userId = user.id;
         token.email = user.email;
         token.name = user.name;
 
-        
         if (account?.provider) {
-          token.loginType = account.provider; 
+          token.loginType = account.provider;
         } else {
-          token.loginType = "credentials";  
+          token.loginType = "credentials";
         }
       }
-      console.log("JWT Token:", token); 
+      console.log("JWT Token:", token);
       return token;
     },
-  
+
     async session({ session, token }) {
       session.user.id = token.userId;
       session.user.email = token.email;
       session.user.name = token.name;
-      session.user.loginType = token.loginType || "credentials";  
+      session.user.loginType = token.loginType || "credentials";
 
-      console.log("Session Data:", session); 
+      console.log("Session Data:", session);
       return session;
     },
-  
-  }
+  },
 };
 
 const handler = NextAuth(authOptions);
